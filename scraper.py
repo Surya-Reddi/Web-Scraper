470e66b2c413dcebb21bcd1abff402f1177980dd
Surya <suryaprasad5858@gmail.com>
2024-09-19 12:56:16 +0530

    extracting quotes and authors

--- scraper.py
+++ scraper.py
@@ -2,20 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-    url = "https://news.ycombinator.com/jobs"
+    url = "https://quotes.toscrape.com"
     response = requests.get(url)
     
-    # print (response.content)
+    # print (response.text)
     soup = BeautifulSoup(response.content, "html.parser")
     
-    elements = soup.find_all(class_ = "titleline")
-    sites = [e.find_next(class_ = "sitestr") for e in elements]
-    
-    print(f"Elements : {len(elements)}")
-    print(f"Sites : {len(sites)}")
-    
-    for e in elements:
-        print(e.get_text())
+    quotes = soup.find_all("span", attrs={"class":"text"})
+    authors = soup.find_all("small", attrs={"class": "author"})
+   
+    for q,a in zip(quotes,authors):
+        print( q.text + "-" + a.text)
     
 if __name__ == "__main__" :
     main()
